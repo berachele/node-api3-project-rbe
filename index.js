c5bb5f32e49b68c719dc777beb8b79c05c683c00
Rachele Edwards <dot.com1472@gmail.com>
2020-05-06 09:33:31 -0600

    can successfully use logger middleware and consoles data correctly

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code away!
+const express = require('express')
+const logger = require('./middleware/logger')
+const server = express()
+
+server.use(express.json())
+
+
+server.use(logger())
+
+server.get('/', (req, res) => {
+    res.json({message: "Why, hello there"})
+})
+
+server.listen(5000, () => {
+    console.log('\n **SERVER IS RUNNING** \n')
+})
