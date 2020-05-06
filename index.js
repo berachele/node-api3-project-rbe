9a8f37edc177c8899794ec93fd7244beb90275fa
Rachele Edwards <dot.com1472@gmail.com>
2020-05-06 09:54:18 -0600

    filled out validateUserId

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 const express = require('express')
 const logger = require('./middleware/logger')
 const server = express()
+const postRouter = require('./posts/postRouter')
+const userRouter = require('./users/userRouter')
 
 server.use(express.json())
 
@@ -11,6 +13,10 @@ server.use(logger())
 server.get('/', (req, res) => {
     res.json({message: "Why, hello there"})
 })
+
+server.use('/api/posts', postRouter)
+
+server.use('/api/users', userRouter)
 
 server.listen(5000, () => {
     console.log('\n **SERVER IS RUNNING** \n')
--- users/userRouter.js
+++ users/userRouter.js
@@ -2,11 +2,13 @@ const express = require('express');
 
 const router = express.Router();
 
+Users = require('./userDb')
+
 router.post('/', (req, res) => {
   // do your magic!
 });
 
-router.post('/:id/posts', (req, res) => {
+router.post('/:id/posts', validateUserId(), (req, res) => {
   // do your magic!
 });
 
@@ -14,26 +16,44 @@ router.get('/', (req, res) => {
   // do your magic!
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', validateUserId(), (req, res) => {
   // do your magic!
 });
 
-router.get('/:id/posts', (req, res) => {
+router.get('/:id/posts', validateUserId(), (req, res) => {
   // do your magic!
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', validateUserId(), (req, res) => {
   // do your magic!
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', validateUserId(), (req, res) => {
   // do your magic!
+
 });
 
 //custom middleware
 
 function validateUserId(req, res, next) {
   // do your magic!
+  Users.getById(req.params.id)
+  .then(user => {
+    if(user){
+      req.user = user
+      next()
+    } else {
+      res.status(400).json({
+        message: "Invalid user ID"
+      })
+    }
+  })
+  .catch(err => {
+    console.log({err})
+    res.status(500).json({
+      error: "There was an error retrieving this user"
+    })
+  })
 }
 
 function validateUser(req, res, next) {
